41caf5dd46767cdd65671ee9a76848661c29312c
Maxim <126081409+Maxim-Levchenko@users.noreply.github.com>
2024-01-22 14:55:01 +0200

    carts-orders-modified-admin-panel

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth, messages
+from django.db.models import Prefetch
 # from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from carts.models import Cart
+from orders.models import Order, OrderItem
 
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
@@ -14,11 +17,18 @@ def login(request):
             username = request.POST['username']
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password) # check in db
+            
+            session_key = request.session.session_key
+            
             if user:
                 auth.login(request, user)
-                messages.success(request, '{{user.username}} successfully logged in')
+                messages.success(request, f'{user.username} successfully logged in')
                 
-                if request.POST.get('next', None): # if not authorized user try to visit profile
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
+                
+                redirect_page = request.POST.get('next', None) # if not authorized user try to visit profile
+                if redirect_page and redirect_page != reverse('user:logout'):
                     return redirect(request.POST.get('next')) # '/user/profile/'
 
                 return redirect(reverse('main:index'))
@@ -37,8 +47,15 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            
+            session_key = request.session.session_key
+            
             user = form.instance
             auth.login(request, user)
+                        
+            if session_key:
+                Cart.objects.filter(session_key=session_key).update(user=user)
+            
             messages.success(request, f'{user.username} successfully signed up')
             return redirect(reverse('main:index'))
     else:
@@ -62,11 +79,22 @@ def profile(request):
     else:
         form = ProfileForm(instance=request.user)
         
+    orders = Order.objects.filter(user=request.user).prefetch_related( # prefetch_related -  т.к. foreignkey идёт от OrderItem к Order (в обратном порядке (не select))
+            Prefetch(
+                "orderitem_set", # additional queryset
+                queryset=OrderItem.objects.select_related("product"),
+            )
+        ).order_by("-id")
+        
     context = {
         'title': 'Profile',
-        'form': form
+        'form': form,
+        'orders': orders
     }
     return render(request, 'users/profile.html', context)
+
+def users_cart(request):
+    return render(request, 'users/users_cart.html')
 
 @login_required
 def logout(request):
